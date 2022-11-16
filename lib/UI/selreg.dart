52f920562fe13286297a1df2ffb65dc834722b77
Jobins Joseph <jobinsjm@gmail.com>
2022-11-16 18:10:44 +0530

    new change

--- lib/UI/selreg.dart
+++ lib/UI/selreg.dart
@@ -11,153 +11,225 @@ class _selreState extends State<selre> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  SingleChildScrollView(
+      body:  Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
 
-        child:
-          Stack(
-            children:[
-              Container(
-            height: 900,
-            decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[Color(0xffff22C1C3),Color(0xffff7d77ff)]
+            )
+        ),
+        child: SingleChildScrollView(
 
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: <Color>[Colors.amber,Colors.tealAccent]
-                )
-            ),
-            ),
-              Column(
-                children: [
+          child:
+            Stack(
+              children:[
 
-              SizedBox(height: 100,),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(child: Text("Registration",style: TextStyle(color: Colors.teal,fontSize: 40),)),
-              ),
-              SizedBox(height: 70,),
-              Column(
-                children: [
-                  SizedBox(height: 30,),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon:Icon(Icons.account_circle_outlined,color: Colors.black,) ,
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(width: 3, color: Colors.black),
+                Column(
+                  children: [
 
+                const SizedBox(height: 100,),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Center(child: Text("Registration",style: TextStyle(color: Colors.white,fontSize: 40),)),
+                ),
+                const SizedBox(height: 70,),
+                Column(
+                  children:  [
+                    SizedBox(height: 30,),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon:Icon(Icons.account_circle_outlined,color: Colors.black,) ,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(width: 3, color: Colors.black),
+
+                          ),
+                            label:Text('Firstname',style: TextStyle(color: Colors.black),)
                         ),
-                          label:Text('Firstname',style: TextStyle(color: Colors.black),)
+
+                      )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.account_circle_outlined,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('Lastname',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.phone,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('Phone No',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.home,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('Address',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.location_on,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('pincode',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.my_location_sharp,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('City',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.my_location_sharp,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('State',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.my_location_sharp,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('Country',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.shopping_bag_outlined,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('ShopName',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.format_list_numbered,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('GST no',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.lock,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('Password',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon:Icon(Icons.lock,color: Colors.black,) ,
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(width: 3, color: Colors.black),
+
+                              ),
+                              label:Text('Confirm password',style: TextStyle(color: Colors.black),)
+                          ),
+
+                        )
+                    ),
+                    SizedBox(height: 50,),
+
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        height: 50,
+                        width: 500,
+                        color: Colors.white38,
+
+                        child: MaterialButton(onPressed: (){},
+                            child:Text('SignUp',style: TextStyle(fontSize: 22),),
+                          splashColor: Colors.teal,
+                        ),
                       ),
-
-                    )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.account_circle_outlined,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('Lastname',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.phone,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('Phone No',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.home,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('Address',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.location_on,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('pincode',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.my_location_sharp,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('City',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.my_location_sharp,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('State',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon:Icon(Icons.my_location_sharp,color: Colors.black,) ,
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(width: 3, color: Colors.black),
-
-                            ),
-                            label:Text('Country',style: TextStyle(color: Colors.black),)
-                        ),
-
-                      )
-                  ),
-
-                ],
-              )
-            ],
-          ),
-      ]
+                    ),
+                  ],
+                )
+              ],
+            ),
+        ]
     ),
+        ),
       ),
       );
   }
